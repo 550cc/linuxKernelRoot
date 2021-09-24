21a47a6bbd44ff2558aea8f87950f7847eb09fba
abcz316 <mailyonghu123@vip.qq.com>
2021-09-25 00:51:39 +0800

    fix error

--- testRoot/main.cpp
+++ testRoot/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <cstdio>
+#include <sstream>
 #include <sys/capability.h>
 
 #include "super_root.h"
@@ -27,11 +28,11 @@ void show_capability_info()
 	FILE * fp = popen("getenforce", "r");
 	if (fp)
 	{
-		char cmd[512] = { 0 };
-		fread(cmd, 1, sizeof(cmd), fp);
+		char shell[512] = { 0 };
+		fread(shell, 1, sizeof(shell), fp);
 		pclose(fp);
 
-		printf("SELinux status: %s\n", cmd);
+		printf("SELinux status: %s\n", shell);
 	}
 }
 void test_root()
@@ -68,12 +69,20 @@ void test_enable_selinux()
 }
 
 
-void test_run_cmd(char * cmd, bool bKeepAdbRoot = false) {
-	printf("inject_cmd_remote_process(%s)\n", cmd);
+void test_run_adb_shell(char * shell, bool bKeepAdbRoot = false) {
+	printf("inject_shell_remote_process(%s)\n", shell);
 	char szResult[0x1000] = { 0 };
-	ssize_t ret = safe_inject_adb_process_run_cmd_wrapper(ROOT_KEY, cmd, bKeepAdbRoot, szResult, sizeof(szResult));
-	printf("inject_cmd_remote_process ret val:%zd\n", ret);
-	printf("inject_cmd_remote_process result:%s\n", szResult);
+	ssize_t ret = safe_inject_adb_process_run_shell_wrapper(ROOT_KEY, shell, bKeepAdbRoot, szResult, sizeof(szResult));
+	printf("inject_shell_remote_process ret val:%zd\n", ret);
+	printf("inject_shell_remote_process result:%s\n", szResult);
+}
+
+void test_run_shell(char * shell) {
+	printf("test_run_shell(%s)\n", shell);
+	char szResult[0x1000] = { 0 };
+	ssize_t ret = safe_run_shell(ROOT_KEY, shell, szResult, sizeof(szResult));
+	printf("test_run_shell ret val:%zd\n", ret);
+	printf("test_run_shell result:%s\n", szResult);
 }
 
 int main(int argc, char *argv[])
@@ -86,8 +95,9 @@ int main(int argc, char *argv[])
 		"\t2.获取ROOT权限\n"
 		"\t3.绕过SELinux\n"
 		"\t4.还原SELinux\n"
-		"\t5.执行ROOT权限级别的Shell命令\n"
-		"\t6.赋予ADB最高级别权限\n"
+		"\t5.执行ROOT Shell命令\n"
+		"\t6.执行ADBShell命令\n"
+		"\t7.赋予ADB最高级别权限\n"
 		"\t新一代root，跟面具完全不同思路，摆脱面具被检测的弱点，完美隐藏root功能，挑战全网root检测手段，兼容安卓APP直接JNI调用，稳定、流畅、不闪退。\n"
 		"======================================================\n"
 	);
@@ -97,51 +107,38 @@ int main(int argc, char *argv[])
 	--argc;
 
 
-	int cmdc;
-	char *cmdv[6];
-
-	while (argc) {
-		// Clean up
-		cmdc = 0;
-		memset(cmdv, 0, sizeof(cmdv));
-
-		// Split the commands
-		for (char *tok = strtok(argv[0], " "); tok; tok = strtok(nullptr, " "))
-		{
-			cmdv[cmdc++] = tok;
-			if (cmdc == 0)
-			{
-				continue;
-			}
-		}
-			
-
-		if (strcmp(cmdv[0], "show") == 0) {
-			show_capability_info();
-		}
-		else if (strcmp(cmdv[0], "root") == 0) {
-			test_root();
-		}
-		else if (strcmp(cmdv[0], "disable") == 0) {
-			test_disable_selinux();
-		}
-		else if (strcmp(cmdv[0], "enable") == 0) {
-			test_enable_selinux();
-		}
-		else if (strcmp(cmdv[0], "cmd") == 0) {
-			test_run_cmd("id");
-			//test_run_cmd("id > /sdcard/run.txt");
-			//test_run_cmd("insmod rwProcMem37.ko > /sdcard/run.txt");
-		}
-		else if (strcmp(cmdv[0], "adb") == 0) {
-			test_run_cmd("id", true);
-		}
-		else {
-			return 1;
-		}
-
-		--argc;
-		++argv;
+	if (strcmp(argv[0], "show") == 0) {
+		show_capability_info();
 	}
+	else if (strcmp(argv[0], "root") == 0) {
+		test_root();
+	}
+	else if (argc >=2 && strcmp(argv[0], "selinux") == 0 && strcmp(argv[1], "disable") == 0) {
+		test_disable_selinux();
+	}
+	else if (argc >= 2 && strcmp(argv[0], "selinux") == 0 && strcmp(argv[1], "enable") == 0) {
+		test_enable_selinux();
+	}
+	else if (argc >= 2 && strcmp(argv[0], "shell") == 0) {
+		std::stringstream sstrCmd;
+		for (int i = 1; i < argc; i++) {
+			sstrCmd << argv[i];
+		}
+		test_run_shell((char*)sstrCmd.str().c_str());
+	}
+	else if (argc > 2 && strcmp(argv[0], "adb") == 0 && strcmp(argv[1], "shell") == 0) {
+		std::stringstream sstrCmd;
+		for (int i = 2; i < argc; i++) {
+			sstrCmd << argv[i];
+		}
+		test_run_adb_shell((char*)sstrCmd.str().c_str());
+	}
+	else if (argc >= 2 && strcmp(argv[0], "adb") == 0 && strcmp(argv[1], "root") == 0) {
+		test_run_adb_shell("id", true);
+	}
+	else {
+		return 1;
+	}
+
 	return 0;
 }
