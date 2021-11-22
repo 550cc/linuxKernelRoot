534f3137bb077afe906049dedf877af168e8ab1a
abcz316 <mailyonghu123@vip.qq.com>
2021-11-23 02:02:03 +0800

    rename adb su

--- su/jni/Android.mk
+++ su/jni/Android.mk
@@ -6,8 +6,8 @@ LOCAL_CFLAGS += -fPIE
 LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
-LOCAL_MODULE    := su
-LOCAL_SRC_FILES :=  ../su.cpp ../base64.cpp ../../testRoot/adb64_helper.cpp ../../testRoot/process64_inject.cpp ../../testRoot/ptrace_arm64_utils.cpp
+LOCAL_MODULE    := adb_su
+LOCAL_SRC_FILES :=  ../adb_su.cpp ../base64.cpp ../../testRoot/adb64_helper.cpp ../../testRoot/process64_inject.cpp ../../testRoot/ptrace_arm64_utils.cpp
 include $(BUILD_EXECUTABLE)
 
 include $(LOCAL_PATH)/simple_su/Android.mk
